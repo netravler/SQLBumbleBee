3b997dbc56fe8e89d85527db6173dd754e90b572
netravler <pheintz@hotmail.com>
2018-09-13 11:45:55 -0500

    Add files via upload

---
+++ SQLBumbleBee/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace SQLBumbleBee
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Server = new System.Windows.Forms.TextBox();
+            this.Database = new System.Windows.Forms.TextBox();
+            this.UserID = new System.Windows.Forms.TextBox();
+            this.Pin = new System.Windows.Forms.TextBox();
+            this.Col = new System.Windows.Forms.TextBox();
+            this.ManualQuery = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Table = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Server
+            // 
+            this.Server.Location = new System.Drawing.Point(17, 34);
+            this.Server.Name = "Server";
+            this.Server.Size = new System.Drawing.Size(100, 20);
+            this.Server.TabIndex = 0;
+            this.Server.Text = "Server";
+            this.toolTip1.SetToolTip(this.Server, "Type in server name");
+            // 
+            // Database
+            // 
+            this.Database.Location = new System.Drawing.Point(17, 70);
+            this.Database.Name = "Database";
+            this.Database.Size = new System.Drawing.Size(100, 20);
+            this.Database.TabIndex = 1;
+            this.Database.Text = "Database";
+            this.toolTip1.SetToolTip(this.Database, "Type in database name");
+            // 
+            // UserID
+            // 
+            this.UserID.Location = new System.Drawing.Point(17, 106);
+            this.UserID.Name = "UserID";
+            this.UserID.Size = new System.Drawing.Size(100, 20);
+            this.UserID.TabIndex = 2;
+            this.UserID.Text = "UserID";
+            this.toolTip1.SetToolTip(this.UserID, "User with rights to the database");
+            // 
+            // Pin
+            // 
+            this.Pin.Location = new System.Drawing.Point(17, 142);
+            this.Pin.Name = "Pin";
+            this.Pin.Size = new System.Drawing.Size(100, 20);
+            this.Pin.TabIndex = 3;
+            this.Pin.Text = "Pin";
+            // 
+            // Col
+            // 
+            this.Col.AcceptsTab = true;
+            this.Col.Location = new System.Drawing.Point(17, 214);
+            this.Col.Multiline = true;
+            this.Col.Name = "Col";
+            this.Col.Size = new System.Drawing.Size(100, 20);
+            this.Col.TabIndex = 5;
+            this.Col.Text = "Col";
+            this.toolTip1.SetToolTip(this.Col, "Initial column to examine");
+            this.Col.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // ManualQuery
+            // 
+            this.ManualQuery.Location = new System.Drawing.Point(142, 215);
+            this.ManualQuery.Name = "ManualQuery";
+            this.ManualQuery.Size = new System.Drawing.Size(822, 20);
+            this.ManualQuery.TabIndex = 6;
+            this.ManualQuery.Text = "ManualQuery";
+            this.toolTip1.SetToolTip(this.ManualQuery, "Enter Query then double click");
+            this.ManualQuery.DoubleClick += new System.EventHandler(this.ManualQuery_DoubleClick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(280, 35);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(684, 160);
+            this.listBox1.TabIndex = 7;
+            this.toolTip1.SetToolTip(this.listBox1, "Double click to run");
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            this.listBox1.MouseHover += new System.EventHandler(this.listBox1_MouseHover);
+            // 
+            // Table
+            // 
+            this.Table.Location = new System.Drawing.Point(17, 178);
+            this.Table.Name = "Table";
+            this.Table.Size = new System.Drawing.Size(100, 20);
+            this.Table.TabIndex = 4;
+            this.Table.Text = "Table";
+            this.toolTip1.SetToolTip(this.Table, "Initial table to examine");
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(985, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.ToolTipText = "Click to exit application";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(142, 35);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 160);
+            this.listBox2.TabIndex = 9;
+            this.toolTip1.SetToolTip(this.listBox2, "Single click on a column");
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            this.listBox2.MouseHover += new System.EventHandler(this.listBox2_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(985, 240);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.Table);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.ManualQuery);
+            this.Controls.Add(this.Col);
+            this.Controls.Add(this.Pin);
+            this.Controls.Add(this.UserID);
+            this.Controls.Add(this.Database);
+            this.Controls.Add(this.Server);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "SQLBumbleBee";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Server;
+        private System.Windows.Forms.TextBox Database;
+        private System.Windows.Forms.TextBox UserID;
+        private System.Windows.Forms.TextBox Pin;
+        private System.Windows.Forms.TextBox Col;
+        private System.Windows.Forms.TextBox ManualQuery;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox Table;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ SQLBumbleBee/Form1.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace SQLBumbleBee
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            ToolTip toolTip1 = new ToolTip();
+            InitializeComponent();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            string Server1 = this.Server.Text;
+            string Username = this.UserID.Text;
+            string Password = this.Pin.Text;
+            string Database = this.Database.Text;
+            string Table = this.Table.Text;
+
+ 
+            string ConnectionString = "Data Source=" + Server1 + ";";
+            ConnectionString += "User ID=" + Username + ";";
+            ConnectionString += "Password=" + Password + ";";
+            ConnectionString += "Initial Catalog=" + Database;
+
+            SqlConnection SQLConnection = new SqlConnection();
+            this.listBox1.Items.Clear();
+            this.listBox1.Items.Add("Working...");
+            try
+            {
+                SQLConnection.ConnectionString = ConnectionString;
+                SQLConnection.Open();
+            }
+            catch (Exception Ex)
+            {
+                // Try to close the connection
+                if (SQLConnection != null)
+                    SQLConnection.Dispose();
+
+                this.listBox1.Items.Add(ConnectionString);
+                this.listBox1.Items.Add(Server1);
+                this.listBox1.Items.Add(UserID);
+                this.listBox1.Items.Add(Pin);
+                this.listBox1.Items.Add(Database);
+                this.listBox1.Items.Add(Col);
+
+                // Create a (useful) error message
+                string ErrorMessage = "A error occurred while trying to connect to the server.";
+                ErrorMessage += Environment.NewLine;
+                ErrorMessage += Environment.NewLine;
+                ErrorMessage += Ex.Message;
+
+                // Show error message (this = the parent Form object)
+                MessageBox.Show(this, ErrorMessage, "Connection error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                // Stop here
+                return;
+            }
+
+            string SQLStatement = "SELECT * FROM " + Table;
+ 
+            // Create a SqlDataAdapter to get the results as DataTable
+            SqlDataAdapter SQLDataAdapter = new SqlDataAdapter(SQLStatement, SQLConnection);
+ 
+            // Create a new DataTable
+            this.listBox1.Items.Clear();
+            DataTable dtResult = new DataTable();
+          
+            // Fill the DataTable with the result of the SQL statement
+            SQLDataAdapter.Fill(dtResult);
+ 
+            // Loop through all entries
+            foreach (DataRow drRow in dtResult.Rows)
+                {
+                // Show a message box with the content of 
+                // the "Name" column
+                    listBox1.Items.Add(drRow[this.Col.Text]);
+                }
+ 
+            // We don't need the data adapter any more
+
+            //DataTable tblSchema = SQLConnection.GetSchema();
+            DataTable tblSchema = SQLConnection.GetSchema(SqlClientMetaDataCollectionNames.Columns);
+          
+            foreach (DataRow drCol in tblSchema.Rows)
+                {
+                    //this.listBox2.Items.Add(drRow[drCol]);
+                    this.listBox2.Items.Add(drCol[3].ToString());
+                }
+
+            SQLDataAdapter.Dispose();
+ 
+            SQLConnection.Close();
+            SQLConnection.Dispose();
+            }
+
+        private void ManualQuery_DoubleClick(object sender, EventArgs e)
+        {
+            string Server1 = this.Server.Text;
+            string Username = this.UserID.Text;
+            string Password = this.Pin.Text;
+            string Database = this.Database.Text;
+            string Table = this.Table.Text;
+
+            string concatstring = "";
+
+            string ConnectionString = "Data Source=" + Server1 + ";";
+            ConnectionString += "User ID=" + Username + ";";
+            ConnectionString += "Password=" + Password + ";";
+            ConnectionString += "Initial Catalog=" + Database;
+
+            SqlConnection SQLConnection = new SqlConnection();
+
+            try
+            {
+                SQLConnection.ConnectionString = ConnectionString;
+                SQLConnection.Open();
+
+                // You can get the server version 
+                // SQLConnection.ServerVersion
+            }
+            catch (Exception Ex)
+            {
+                // Try to close the connection
+                if (SQLConnection != null)
+                    SQLConnection.Dispose();
+
+                this.listBox1.Items.Add(ConnectionString);
+                this.listBox1.Items.Add(Server1);
+                this.listBox1.Items.Add(UserID);
+                this.listBox1.Items.Add(Pin);
+                this.listBox1.Items.Add(Database);
+                this.listBox1.Items.Add(Col);
+
+                // Create a (useful) error message
+                string ErrorMessage = "A error occurred while trying to connect to the server.";
+                ErrorMessage += Environment.NewLine;
+                ErrorMessage += Environment.NewLine;
+                ErrorMessage += Ex.Message;
+
+                // Show error message (this = the parent Form object)
+                MessageBox.Show(this, ErrorMessage, "Connection error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                // Stop here
+                return;
+            }
+
+            string SQLStatement = this.ManualQuery.Text;
+
+            // Create a SqlDataAdapter to get the results as DataTable
+            SqlDataAdapter SQLDataAdapter = new SqlDataAdapter(SQLStatement, SQLConnection);
+
+            // Create a new DataTable
+            DataTable dtResult = new DataTable();
+                         
+            try
+            {
+                // Fill the DataTable with the result of the SQL statement
+                SQLDataAdapter.Fill(dtResult);
+                this.listBox1.Items.Clear();
+
+                // Loop through all entries
+                foreach (DataRow drRow in dtResult.Rows)
+                {
+                  //listBox1.Items.Add(drRow[this.Col.Text]);
+                  //listBox1.Items.Add(drRow);
+                    foreach (var item in drRow.ItemArray)
+                    {
+                        concatstring = concatstring + item + ", ";
+                    }
+                    listBox1.Items.Add(concatstring);
+                    concatstring = "";
+                }
+             }
+            catch (Exception Ex)
+            {
+                // Create a (useful) error message
+                string ErrorMessage = "A error occurred while trying to connect to the server.";
+                ErrorMessage += Environment.NewLine;
+                ErrorMessage += Environment.NewLine;
+                ErrorMessage += Ex.Message;
+
+                // Show error message (this = the parent Form object)
+                MessageBox.Show(this, ErrorMessage, "Connection error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                // Stop here
+                return;
+            }
+            // We don't need the data adapter any more
+            SQLDataAdapter.Dispose();
+
+            SQLConnection.Close();
+            SQLConnection.Dispose();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Greetings I was created by Paul R. Heintz as a demo program");
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string Server1 = this.Server.Text;
+            string Username = this.UserID.Text;
+            string Password = this.Pin.Text;
+            string Database = this.Database.Text;
+            string Table = this.Table.Text;
+            this.Col.Text = this.listBox2.SelectedItem.ToString();
+
+            string ConnectionString = "Data Source=" + Server1 + ";";
+            ConnectionString += "User ID=" + Username + ";";
+            ConnectionString += "Password=" + Password + ";";
+            ConnectionString += "Initial Catalog=" + Database;
+
+            SqlConnection SQLConnection = new SqlConnection();
+
+            try
+            {
+                SQLConnection.ConnectionString = ConnectionString;
+                SQLConnection.Open();
+                listBox1.Items.Clear();
+            }
+            catch (Exception Ex)
+            {
+                // Try to close the connection
+                if (SQLConnection != null)
+                    SQLConnection.Dispose();
+
+                this.listBox1.Items.Add(ConnectionString);
+                this.listBox1.Items.Add(Server1);
+                this.listBox1.Items.Add(UserID);
+                this.listBox1.Items.Add(Pin);
+                this.listBox1.Items.Add(Database);
+                this.listBox1.Items.Add(Col);
+
+                // Create a (useful) error message
+                string ErrorMessage = "A error occurred while trying to connect to the server.";
+                ErrorMessage += Environment.NewLine;
+                ErrorMessage += Environment.NewLine;
+                ErrorMessage += Ex.Message;
+
+                // Show error message (this = the parent Form object)
+                MessageBox.Show(this, ErrorMessage, "Connection error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                // Stop here
+                return;
+            }
+
+            string SQLStatement = "SELECT * FROM " + Table;
+            
+            // Create a SqlDataAdapter to get the results as DataTable
+            SqlDataAdapter SQLDataAdapter = new SqlDataAdapter(SQLStatement, SQLConnection);
+
+            // Create a new DataTable
+            DataTable dtResult = new DataTable();
+
+            // Fill the DataTable with the result of the SQL statement
+            SQLDataAdapter.Fill(dtResult);
+
+            try
+            {
+                // Loop through all entries
+                foreach (DataRow drRow in dtResult.Rows)
+                {
+                    // Show a message box with the content of 
+                    // the "Name" column
+                    //MessageBox.Show(drRow["Name"].ToString());
+                    listBox1.Items.Add(drRow[this.Col.Text]);
+                }
+            }
+            catch (Exception Ex)
+            {
+                // Create a (useful) error message
+                string ErrorMessage = "A error occurred while trying to connect to the server.";
+                ErrorMessage += Environment.NewLine;
+                ErrorMessage += Environment.NewLine;
+                ErrorMessage += Ex.Message;
+
+                // Show error message (this = the parent Form object)
+                MessageBox.Show(this, ErrorMessage, "Connection error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                // Stop here
+                return;
+            }
+            SQLDataAdapter.Dispose();
+            SQLConnection.Close();
+            SQLConnection.Dispose();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void listBox2_MouseHover(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+            
+        }
+
+        private void listBox1_MouseHover(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+                    
+  }
+
+}
